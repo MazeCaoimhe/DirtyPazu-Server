4ea1301cd26bf92139cf073eacfe47bc2980a794
Mario <mario.vieilledent@gmail.com>
2021-11-07 12:50:09 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,85 @@
+const e = require('cors');
+var express = require('express');
+var app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origins: ['http://localhost:4200']
+    }
+});
+const port = process.env.PORT || 5000;
+ 
+app.set(port, process.env.PORT);
+app.use(express.static('./client/'));
+ 
+// MongoDB
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectID;
+const uri = "mongodb+srv://Pazu:ufn0ddI1m5f04KWW@pazucluster.klrce.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const clusterName = 'PazuSarl';
+const collectionName = 'DibiDictonary';
+
+client.connect(async (err) => {
+    if (err) throw err;
+});
+
+// Exemple
+io.on('connection', socket => {
+
+    socket.on('fetchDict', () => {
+        console.log('Récupération du dico');
+        client.db(clusterName).collection(collectionName).find().toArray((err, res) => {
+            if (err) throw err;
+            socket.emit('loadDict', {dict: res});
+        });
+    });
+
+    socket.on('addWord', word => {
+        if (!word.dibi || !word.french) {
+            const mes = 'Impossible d\'ajouter le mot ' + word.dibi + ' car il faut au moins le mot en Dibi et sa tradution en Français'
+            console.log(mes);
+            socket.emit('responseAddWord', {status: 2, mes: mes });
+        } else {
+            console.log('Ajout d\'un mot : ' + word.dibi);
+            try {
+                client.db(clusterName).collection(collectionName).insertOne(word);
+            } catch (e) {
+                socket.emit('responseAddWord', {status: 1, mes: 'Erreur dans l\'enregistrement du mot : ' + e.message });
+                throw e;
+            }
+            socket.emit('responseAddWord', {status: 0, mes: word.dibi + ' enregistré avec succès.' });
+        }
+    });
+
+    socket.on('editWord', word => {
+        if (!word.dibi || !word.french) {
+            const mes = 'Impossible de modifier le mot ' + word.dibi + ' car il faut au moins le mot en Dibi et sa tradution en Français'
+            console.log(mes);
+            socket.emit('responseEditWord', {status: 2, mes: mes });
+        } else {
+            console.log(JSON.stringify(word, null, 3));
+            try {
+                client.db(clusterName).collection(collectionName).updateOne({_id: ObjectId(word._id)}, {$set: {dibi: word.dibi, french: word.french, english: word.english, partOfSpeech: word.partOfSpeech, author: word.author, date: word.date, description: word.description}}, (err, res) => {console.log(res);}, false);
+            } catch (e) {
+                socket.emit('responseEditWord', {status: 1, mes: 'Erreur dans la modification du mot : ' + e.message });
+                throw e;
+            }
+            socket.emit('responseEditWord', {status: 0, mes: word.dibi + ' modifié avec succès.' });
+        }
+    });
+
+    socket.on('deleteWord', word => {
+        console.log('Suppression d\'un mot : ' + word.dibi);
+        try {
+            client.db(clusterName).collection(collectionName).deleteOne({_id: ObjectId(word._id)});
+        } catch (e) {
+            throw e;
+        }
+    })
+
+});
+ 
+http.listen(port, () => {
+    console.log('App listening on port ' + port);
+});
